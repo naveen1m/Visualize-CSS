9f03807bc51cdb30123176e94634523e67a888a8
naveen <stupiddint@gmail.com>
2023-09-22 19:13:07 +0530

    Add: intro page

---
+++ src/pages/GetStartedPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../components/sidebar/Sidebar'
+
+function GetStartedPage() {
+    return (
+        <div>
+
+            <Sidebar />
+            <h1 className='text-3xl'>Hello</h1>
+        </div>
+    )
+}
+
+export default GetStartedPage
