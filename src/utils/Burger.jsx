c6ee3333c5bf1346ba29354194ed745de09f4c22
naveen <stupiddint@gmail.com>
2023-09-24 12:21:33 +0530

    removed mantine hamburger

--- src/utils/Burger.jsx
+++
@@ -1,7 +0,0 @@
-import { useDisclosure } from '@mantine/hooks';
-import { Burger } from '@mantine/core';
-
-export function Hamburger() {
-    const [opened, { toggle }] = useDisclosure();
-    return <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />;
-}
