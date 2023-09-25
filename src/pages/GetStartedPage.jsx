660681da28882b7fdfcd3491e6d6088f1b8dbd70
naveen <stupiddint@gmail.com>
2023-09-25 10:07:52 +0530

    page styled

--- src/pages/GetStartedPage.jsx
+++ src/pages/GetStartedPage.jsx
@@ -1,13 +1,29 @@
-import React, { useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
 import SidebarContext from '../context/sidebarcontext'
 function GetStartedPage() {
-    const {sidebar} = useContext(SidebarContext);
+    const { sidebar } = useContext(SidebarContext);
     return (
-        <div>
-            {sidebar?<Sidebar />:""}
-            <h1 className='text-3xl'>Hello</h1>
-        </div>
+        <Fragment>
+            <div className='flex flex-row max-w-full'>
+                <section>
+                    {sidebar ? <Sidebar /> : ""}
+                </section>
+
+                <section className={`content ml-2 ${sidebar ? " ml-4" : ""}`}>
+                    <h1 className={`text-3xl font-bold ${sidebar ? "text-violet-400" : ""}`}>Get start with the VisualizeCSS!</h1>
+                    <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam praesentium magni suscipit quidem repellendus reprehenderit? Mollitia fugiat quaerat provident, unde adipisci reprehenderit excepturi ut, eos sapiente, minus minima earum praesentium?</p>
+                    <h2 className='text-2xl mt-2 font-semibold'>How you can achive more from this?</h2>
+                    <p className="text-xl ">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolores obcaecati excepturi, illum distinctio voluptates, culpa dolorum assumenda aspernatur ab iusto dignissimos nulla provident vitae, quia corporis! Aperiam, saepe alias.
+                    </p>
+                    <h2 className='text-2xl mt-2 font-semibold'>Estimated time to learn and visualizeCSS important topics from this website</h2>
+                    <p className="text-xl ">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolores obcaecati excepturi, illum distinctio voluptates, culpa dolorum assumenda aspernatur ab iusto dignissimos nulla provident vitae, quia corporis! Aperiam, saepe alias.
+                    </p>
+                </section>
+            </div>
+        </Fragment>
     )
 }
 
