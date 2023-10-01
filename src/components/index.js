6c5ab0c5c2427beb16d8d488cb22ffa61bdbe6d8
naveen <stupiddint@gmail.com>
2023-10-01 09:37:49 +0530

    modified and update

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
 export { default as Carousel } from './Carousel'
 export { default as Navbar } from './Navbar'
+export { default as CopyToClipboard } from './CopyToClipboard'
+export { default as RGBcolor } from './colors/RGBcolor'
+export { default as Hexcolor } from './colors/Hexcolor'
+export { default as RGBAcolor } from './colors/RGBAcolor'
+export { default as HSLcolor } from './colors/HSLcolor'
+export { default as HSLAcolor } from './colors/HSLAcolor'
--- src/pages/contents/index.js
+++ src/pages/contents/index.js
@@ -4,7 +4,4 @@ export { default as Display } from './Display'
 export { default as Fonts } from './Fonts'
 export { default as Colors } from './Colors'
 
-export { default as CssSelector } from "./CssSelector";
-export { default as Display } from "./Display";
-export { default as Fonts } from "./Fonts";
 
