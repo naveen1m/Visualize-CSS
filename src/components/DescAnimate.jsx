e0f135ad66275feae263ee86434729f490eca8c3
naveen <stupiddint@gmail.com>
2023-10-02 16:12:58 +0530

    update: css

--- src/components/DescAnimate.jsx
+++ src/components/DescAnimate.jsx
@@ -1,18 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { TypeAnimation } from 'react-type-animation';
 
-function DescAnimate({ text }) {
+function DescAnimate({ desc }) {
+    const [key, setKey] = useState(0);
+
+    useEffect(() => {
+        setKey((prevKey) => prevKey + 1);
+    }, [desc]);
+
     return (
         <Fragment>
-            <TypeAnimation
-                // aria-label={text}
-                sequence={text}
-                wrapper="span"
-                speed={50}
-                style={{ fontSize: '1.5em', display: 'inline-block', overflow: 'hidden', }}
-                repeat={1}
-                cursor={false}
-            />
+            <div className=' bg-[#3a404d] rounded-md text-white '>
+                <p className='text-sm pl-6 font-mono font-normal'>Insights</p>
+                <div className='max-w-2xl min-w-[25rem] min-h-fit bg-blue-950 rounded overflow-hidden text-stone-50 px-4 py-2 '>
+                    <p className='text-sm'>
+                        <TypeAnimation
+                            // aria-label={desc}
+                            key={key}
+                            sequence={desc}
+                            wrapper="span"
+                            speed={60}
+                            style={{ display: 'inline-block', overflow: 'hidden', }}
+                            repeat={1}
+                            cursor={false}
+                        />
+                    </p>
+                </div>
+            </div>
         </Fragment>
     )
 }
