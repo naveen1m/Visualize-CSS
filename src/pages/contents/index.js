6697f5bf74ac8f7b5269461f3f1ccbfacf011b71
naveen <stupiddint@gmail.com>
2023-10-03 09:59:14 +0530

    updated

--- src/pages/contents/index.js
+++ src/pages/contents/index.js
@@ -1,4 +1,4 @@
-
+export { default as GetStartedPage } from './GetStartedPage'
 export { default as CssSelector } from './CssSelector'
 export { default as Display } from './Display'
 export { default as Fonts } from './Fonts'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export { default as Home } from './Home'
-export { default as GetStartedPage } from './GetStartedPage'
 export { default as About } from './About'
 export { default as ErrorPage } from './ErrorPage'
