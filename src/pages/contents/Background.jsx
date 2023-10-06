4590c5521c1f3dca64c9860279f347e6cb4d9dd6
naveen <stupiddint@gmail.com>
2023-10-06 08:32:31 +0530

    background page inititalized

---
+++ src/pages/contents/Background.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+function Background() {
+    return (
+        <Fragment>
+            <p className="text-center bg-violet-400">Learn all background properties here and play with it</p>
+        </Fragment>
+    )
+}
+
+export default Background
