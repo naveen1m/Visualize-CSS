4c6f3fcf76e6623e31d8b3ce8f1c5e53a40228f8
naveen <stupiddint@gmail.com>
2023-10-07 08:05:48 +0530

    bg logic started

--- src/pages/contents/Background.jsx
+++ src/pages/contents/Background.jsx
@@ -4,6 +4,72 @@ function Background() {
     return (
         <Fragment>
             <p className="text-center bg-violet-400">Learn all background properties here and play with it</p>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background color</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background gradient</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background Image & multiple bg-image</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background shorthand</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background size</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background position</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background color</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background origin property</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background Attachment</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background clip or bg-property</h1>
+                <div>
+
+                </div>
+            </section>
+            <section className='my-2'>
+                <h1 className='bg-violet-400 pl-3 text-xl font-serif font-bold text-white'>Background video or gif</h1>
+                <div>
+
+                </div>
+            </section>
         </Fragment>
     )
 }
