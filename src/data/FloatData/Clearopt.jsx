2d4b1df8968dc61af0612ed852549c9c21752fc4
Pritam Kumar Sarangi <kumarsarangipritam@gmail.com>
2023-10-09 09:12:45 +0530

    Adding Float and clear options in data folder

---
+++ src/data/FloatData/Clearopt.jsx
@@ -0,0 +1,29 @@
+const Clearopt =[
+    
+      
+    {
+    name: 'none',
+    display: `clear: none;`,
+    syntax: `It is a keyword indicating that the element is not moved down to clear past floating elements.`
+   },
+   {
+    name: 'left',
+    display: `clear: left;`,
+    syntax: `The element will not be allowed to float on the left side of any floated elements. It will move below any floated elements on the left.`
+   },
+   {
+    name: 'right',
+    display: `clear: right;`,
+    syntax: `The element will not be allowed to float on the right side of any floated elements. It will move below any floated elements on the right.`
+   },
+   {
+    name: 'both',
+    display: `clear: both;`,
+    syntax: `The element will not be allowed to float on either side of any floated elements. It will move below any floated elements on both sides.`
+   },
+   
+   
+  
+
+];
+export default Clearopt;
