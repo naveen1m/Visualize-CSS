11bf9f50b27fbf49019eb0512b8d92452e4052f9
naveen <stupiddint@gmail.com>
2023-10-11 11:42:29 +0530

    image page initialised

---
+++ src/pages/contents/Images.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+function Images() {
+    return (
+        <Fragment>
+            <p className="text-center bg-violet-400">Learn all Image properties here and play with it</p>
+        </Fragment>
+    )
+}
+
+export default Images
