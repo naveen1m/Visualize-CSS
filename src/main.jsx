8835097fbbcf2e1df6ee5bbf1336da516d51f3f0
naveen <stupiddint@gmail.com>
2024-05-05 11:57:10 +0530

    redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import store from './app/store.js'
+import { Provider } from 'react-redux'
 import AppWrapper from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppWrapper />
+    <Provider store={store}>
+      <AppWrapper />
+
+    </Provider>
   </React.StrictMode>,
 )
