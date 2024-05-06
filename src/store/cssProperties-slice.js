058911605f2ca87a5bfcbe0b2dabf9cef23d004b
Pritam Kumar Sarangi <kumarsarangipritam@gmail.com>
2024-05-06 09:09:21 +0530

    update: initital value and activeValue

--- src/store/cssProperties-slice.js
+++ src/store/cssProperties-slice.js
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 export const cssPropertiesSlice = createSlice({
     name: "cssProperties",
     initialState:{
-        data: []
+        data: [],
+        activeValue: 0
     },
     reducers: {
         updateData: (state, action)=>{
             state.data = action.payload;
-        }
+        },
+        selectData: (state, action)=>{
+            state.activeValue = action.payload;
+        } 
     }
 })
 
-export const {updateData} = cssPropertiesSlice.actions;
+export const {updateData, selectData} = cssPropertiesSlice.actions;
 export default cssPropertiesSlice.reducer;
