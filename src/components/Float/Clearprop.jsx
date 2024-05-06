053e4ceeb2906e54d7759d570df12702810c3358
Pritam Kumar Sarangi <kumarsarangipritam@gmail.com>
2024-05-06 14:43:09 +0530

    update : clear component

--- src/components/Float/Clearprop.jsx
+++ src/components/Float/Clearprop.jsx
@@ -3,32 +3,19 @@ import floatAndClear from "../../database/FloatAndClear.json";
 import CSSPlayground from "../shared/CSSPlayground";
 import DescriptionBox from "../shared/DescriptionBox";
 import { useSelector } from "react-redux";
-import {
-  Container,
-  OptionsBox,
-  OptionItem,
-  TextContainer,
-  DescriptionContainer,
-} from "../../assets/StyledComponents";
 
-/**
- *
- * data = [ "float: left", "float:right", "float:none" ]
- */
 
 function Clearprop() {
   const value = useSelector((state)=> state.cssProperties.activeValue);
   const data = floatAndClear.Clear.keyword.map(
     (keyword) => `clear: ${keyword}`
   );
-
-  const keyvalue =  data[value].split(":");
-  console.log( keyvalue)
-  // let keyword = ["none","left"]
-  // const keywordDesc = floatAndClear.Clear?.keywordDescription[`${keyvalue[1]}`];
-  // console.log(keywordDesc)
-  // console.log("keywordDesc",keyword,  keywordDesc)
-  const descText = <>
+  // console.log(data)
+  const [key, keyvalue] =  data[value].split(":"); 
+  let trimmedkeyvalue = keyvalue.trim();
+  const keywordDesc = floatAndClear.Clear.keywordDescription[trimmedkeyvalue];
+  // console.log("keyword desc is : ",keywordDesc);
+  const descrText = <>
   <p className=' text-xl font-serif tracking-wider text-black  pl-4  ml-12 mt-4'>The <span className='text-red-500'>clear</span> property controls the flow next to floated elements.</p>
   <p className=' text-xl font-serif tracking-wider text-black  pl-4  ml-12 mt-4'>The <span className='text-red-500'>clear</span> property specifies what should happen with the element that is next to a floating element.</p>
   </>
@@ -39,14 +26,14 @@ function Clearprop() {
     
 <h1 className='font-serif tracking-wider text-black  pl-4 font-bold  ml-12 mt-12'>Clear</h1>
 
-    <DescriptionBox descText={descText} />
+    <DescriptionBox title={"Info"} descText={descrText} />
       <CSSPlayground
         data={data}
         component={ClearComponent}
         screenDesc="What's happening on the screen, This is showing the clear properties live interation effect"
         bgColor="beige"
       />
-      <DescriptionBox title={"Keyword description"} descText={descText} />
+      <DescriptionBox title={"Keyword description"} descText={keywordDesc} />
     </>
   );
 }
@@ -56,6 +43,7 @@ export default Clearprop;
 const ClearComponent = () => {
   const value = useSelector((state)=> state.cssProperties.activeValue)
   const data = useSelector((state)=> state.cssProperties.data);
+  // console.log("val", value, "data",data)
   const [clear, keyword] =  data[value].split(":");
 
 
